dc4c2625bc85497cbe74b68b544d5b300a6783f3
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-14 17:13:33 -0300

    feat: add customer existance validation

--- src/index.js
+++ src/index.js
@@ -6,16 +6,23 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(3333)
+app.listen(3333);
 
 const customers = [];
 
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body;
 
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-  customers.push({ cpf, name, id, statement: [] });
+  if (customerAlreadyExists)
+    return res
+      .status(400)
+      .json({ error: 'Customer already exists', status: '400' });
+
+  customers.push({ cpf, name, id: uuidv4(), statement: [] });
 
   return res.status(201).send();
 });
