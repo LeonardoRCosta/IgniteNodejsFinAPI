cbef5c6f3943570af2240e85e02ad1da3b41318e
LeonardoRCosta <rm94286@fiap.com.br>
2022-12-14 19:28:06 -0300

    feat: create update account & get account.

--- src/index.js
+++ src/index.js
@@ -111,4 +111,18 @@ app.post('/withdraw', verifyIfAccountExists, (req, res) => {
   return res.status(201).send();
 });
 
+app.put('/account', verifyIfAccountExists, (req, res) => {
+  const { name } = req.body;
+  const { customer } = req;
+
+  customer.name = name;
+
+  return res.status(201).send();
+});
+
+app.get('/account', verifyIfAccountExists, (req, res) => {
+  const { customer } = req;
+
+  return res.json(customer);
+});
 app.listen(3333);
